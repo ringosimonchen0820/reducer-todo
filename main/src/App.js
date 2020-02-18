e34485d44698853ffe28e074b6aa8f6c6c494278
ringosimonchen0820 <ringosimonchen0820@gmail.com>
2020-02-18 12:07:20 -0500

    implement reducer into app.js

--- main/src/App.js
+++ main/src/App.js
@@ -1,24 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useReducer } from 'react';
+import TodoList from './components/TodoList'
 import './App.css';
 
+// import reducers
+import {initialState, reducer } from './reducer/reducerTodo'
+
+
+
+
 function App() {
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const [item, setItem] = useState('')
+
+  // ** LIST FORM
+  const handleChanges = event => {
+    setItem(event.target.value)
+  }
+  const submitItem = event => {
+    event.preventDefault();
+  }
+
+  // ** TOGGLE COMPLETED
+  const toggleItem = (id) => {
+    dispatch( { type:"TOGGLE_ITEM", payload: id})
+  }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TodoList state={state.todos} toggleItem={toggleItem}/>
+      {/* <ListForm addItem={addItem} /> */}
+
+
+      <form onSubmit={submitItem}>
+        <input
+          type="text"
+          name="item"
+          value={item}
+          onChange={handleChanges}
+          placeholder="Add item"
+        />
+        <button onClick={() => dispatch({ type: 'ADD_TODO', payload: item })}>Add Item</button>
+      </form>
+
+      <button onClick={() => dispatch({type:"CLEAR_COMPLETED"})}>Clear Completed</button>
     </div>
   );
 }
